d4ecafe4fae66bab17bee936735b3ebd5c658d62
Ernestico <developer1575@gmail.com>
2024-07-17 17:57:19 -0400

    DEploy 4

---
+++ dota-rank/src/app/angular.app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { join } from 'path';
+
+@Controller()
+export class AngularAPpControllers {
+  constructor() {}
+
+  @Get('*')
+  serveAngularApp(@Res() res: Response) {
+    const indexHtml = join(__dirname,'..', 'angular-app');
+    res.sendFile(indexHtml+"/index.html");
+  }
+
+}
